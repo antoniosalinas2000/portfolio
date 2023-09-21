cf01109fb38ed8e169a490d99b6c4d3864f8bc31
Antonio Salinas <antonio.salinas@visma.com>
2023-09-20 20:09:38 -0500

    hamburguer

--- components/dropDownMenu.tsx
+++ components/dropDownMenu.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 function DropdownMenu(props: { isOpen: boolean, currentPage : string }) {
     if (!props.isOpen) return null;
 
     return (
         <div className="absolute right-0 w-48 font-iceland text-2xl bg-asr-purple/50 shadow-lg rounded-lg">
-            {props.currentPage != "Inicio" && <a href="/.." className="block px-4 py-2 hover:bg-asr-purple/20 rounded-t-lg">Inicio</a>}
-            {props.currentPage != "Proyectos" && <a href="/projects" className="block px-4 py-2 hover:bg-asr-purple/20">Proyectos</a>}
-            {props.currentPage != "Diseño" && <a href="/design" className="block px-4 py-2 hover:bg-asr-purple/20 rounded-b-lg">Diseño</a>}
+            {props.currentPage != "Inicio" && <Link href="/.." className="block px-4 py-2 hover:bg-asr-purple/20 rounded-t-lg">Inicio</Link>}
+            {props.currentPage != "Proyectos" && <Link href="/projects" className="block px-4 py-2 hover:bg-asr-purple/20">Proyectos</Link>}
+            {props.currentPage != "Diseño" && <Link href="/design" className="block px-4 py-2 hover:bg-asr-purple/20 rounded-b-lg">Diseño</Link>}
         </div>
     );
 }
