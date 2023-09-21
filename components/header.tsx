1bffabde5c4544eddeeab052de52bcef8ea7cbac
Antonio Salinas <antonio.salinas@visma.com>
2023-09-20 20:11:18 -0500

    responsive

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export default function Header(props: {currentPage:string }) {
 
     return (
         <div className="flex flex-row w-full items-center justify-between px-16 py-6">
-            <h1 className="text-4xl font-iceland">Antonio Salinas</h1>
+            <h1 className="text-xl lg:text-4xl font-iceland">Antonio Salinas</h1>
             <div className="flex flex-row items-center justify-center gap-x-4 relative">
                 <h1 className="text-2xl font-iceland">{props.currentPage}</h1>
                 <div className="relative">
