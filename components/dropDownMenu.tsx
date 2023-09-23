ae621336df7090fdb149b37a0ff0275cd5b4bfa2
antoniosalinas2000 <salinas.antonio1820@gmail.com>
2023-09-23 10:15:55 -0500

    change user

--- components/dropDownMenu.tsx
+++ components/dropDownMenu.tsx
@@ -7,7 +7,7 @@ function DropdownMenu(props: { isOpen: boolean, currentPage : string }) {
         <div className="absolute right-0 w-48 font-iceland text-2xl bg-asr-purple/50 shadow-lg rounded-lg">
             {props.currentPage != "Inicio" && <Link href="/.." className="block px-4 py-2 hover:bg-asr-purple/20 rounded-t-lg">Inicio</Link>}
             {props.currentPage != "Proyectos" && <Link href="/projects" className="block px-4 py-2 hover:bg-asr-purple/20">Proyectos</Link>}
-            {/*props.currentPage != "Diseño" && <Link href="/design" className="block px-4 py-2 hover:bg-asr-purple/20 rounded-b-lg">Diseño</Link>*/}
+            {props.currentPage != "Sobre mí" && <Link href="/about-me" className="block px-4 py-2 hover:bg-asr-purple/20 rounded-b-lg">Sobre mí</Link>}
         </div>
     );
 }
